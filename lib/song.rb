7b9e47c099d67dc8bc593adbbabab3edb8d1f52a
tylerselldesign@gmail.com <tylerselldesign@gmail.com>
2018-09-03 20:05:26 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,12 +7,13 @@ class Song
   @@genre_count = {}
   @@artist_count = {}
   
-  def initialize(song, artist, genre)
+  def initialize(name, artist, genre)
     @@count += 1 
     @@artists << artist unless @@artists.any? { |a| a == artist}
     @@genres << genre unless @@genres.any? { |g| g == genre}
     @artist = artist
     @genre = genre
+    @name = name
   end
   
   def self.count
